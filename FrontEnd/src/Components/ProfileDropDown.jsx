6938abf222a911e408138900286a6e0a9f7bf9d3
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-09-04 13:52:11 +0530

    logout button functionality

--- FrontEnd/src/Components/ProfileDropDown.jsx
+++ FrontEnd/src/Components/ProfileDropDown.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
-import { BiFile, BiHeart,  BiLogOut, BiNotification } from "react-icons/bi";
+import Link from "next/link";
+import { BiFile, BiHeart, BiLogOut, BiNotification } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
 
-const ProfileDropDown = ({show ,setShow}) => {
-
+const ProfileDropDown = ({ show, setShow }) => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth.userData);
   return (
     <>
       <div className="hs-dropdown relative inline-flex">
         <button
           type="button"
-          className="  ps-1 scale-95 hover:scale-100 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+          className="  p-1 scale95 hover:scale-100 inline-flex  items-center gap-x-2 text-sm font-medium rounded-full border  bg-white text-gray-800 shadow-sm  focus:outline-none  disabled:opacity-50 disabled:pointer-events-none"
           onClick={() => setShow(!show)}
         >
           <img
@@ -19,47 +21,44 @@ const ProfileDropDown = ({show ,setShow}) => {
         </button>
         {show ? (
           <>
-              <div
-                className="rounded-xl shadow-lg bg-white absolute top-full w-44 mt-2 z-10 right-1"
-              >
-                <ul className="py-2">
-                  <li>
-                    <a
-                      className="flex items-center gap-3 px-6 py-2 hover:bg-gray-100 text-gray-900 font-medium"
-                    >
-                      <BiFile />
-                      Blogs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center gap-3  px-6 py-2 hover:bg-gray-100 text-gray-900 font-medium"
-                     
-                    >
-                      <BiNotification />
-                      Notifications
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center gap-3 px-6 py-2 hover:bg-gray-100 text-gray-900 font-medium"
-                      href="javascript:;"
-                    >
-                      <BiHeart />
-                      Likes
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="flex items-center gap-3 px-6 py-2 hover:bg-gray-100 text-red-500 font-medium"
-                      href="javascript:;"
-                    >
-                     <BiLogOut/>
-                      Log Out
-                    </a>
-                  </li>
-                </ul>
-              </div>
+            <div className="rounded-xl shadow-lg bg-white absolute top-full w-44 mt-2 z-10 right-1">
+              <ul className="py-2">
+                <li>
+                  <a className="flex items-center gap-3 px-6 py-2 hover:bg-gray-100 text-gray-900 font-medium">
+                    <BiFile />
+                    Blogs
+                  </a>
+                </li>
+                <li>
+                  <a className="flex items-center gap-3  px-6 py-2 hover:bg-gray-100 text-gray-900 font-medium">
+                    <BiNotification />
+                    Notifications
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="flex items-center gap-3 px-6 py-2 hover:bg-gray-100 text-gray-900 font-medium"
+                    href="javascript:;"
+                  >
+                    <BiHeart />
+                    Likes
+                  </a>
+                </li>
+                <li>
+                  <Link
+                    className="flex items-center gap-3 px-6 py-2 hover:bg-gray-100 text-red-500 font-medium"
+                    href="/"
+                    onClick={() => {
+                      dispatch(logoutUser());
+                      // window.location.reload()
+                    }}
+                  >
+                    <BiLogOut />
+                    Log Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </>
         ) : null}
       </div>
